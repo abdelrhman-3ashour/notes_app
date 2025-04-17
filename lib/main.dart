6c25fe4637660a4b7c7aa1f71cd335f44eb77383
abdelrhman-3ashour <abdo01551046780@gmail.com>
2025-04-17 16:22:25 +0200

    change them and create notes

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,6 @@ class notesapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(theme: ThemeData.dark(), home: notesapp());
   }
 }
