cdfc349ec9d971490f9eb2fe95dad350b97d77a3
abdelrhman-3ashour <abdo01551046780@gmail.com>
2025-04-19 11:34:38 +0200

    create cotes list view

---
+++ lib/widgets/notes_list_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:np/widgets/custom_note_item.dart';
+
+class noteslistview extends StatelessWidget {
+  const noteslistview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: CustomNoteItem(),
+        );
+      },
+    );
+  }
+}
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:np/widgets/coustom_appbar.dart';
-import 'package:np/widgets/custom_note_item.dart';
+import 'package:np/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -9,7 +9,9 @@ class NotesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-      child: Column(children: [CoustomAppbar(), CustomNoteItem()]),
+      child: Column(
+        children: [CoustomAppbar(), Expanded(child: noteslistview())],
+      ),
     );
   }
 }
